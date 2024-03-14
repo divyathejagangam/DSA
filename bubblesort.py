0fd88175e569f8b11f583833e96b2a4cd216f953
divyathejagangam <divyatheja.190@gmail.com>
2024-03-14 14:52:07 -0500

    modified bubblesort
    modified bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,9 +1,13 @@
 def bubble_sort(array):
     n = len(array)
     for i in range(n-1):
+        swap_flag = False
         for j in range(n-1-i):
             if (array[j] > array[j+1]):
                 array[j], array[j+1] = array[j+1] , array[j]
+                swap_flag = True
+        if not swap_flag:
+            break
     return array
 
 mylist=[3,4,6,6,7,5,3,1]
